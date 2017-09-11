cadfae48eece70e718eb4b0d89afa07bb8274f5a
lnzd17 <lindsaykayedutton@gmail.com>
2017-09-11 18:38:45 -0400

    added controllers

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+
+app.use(require('./controllers'));
+
 const port = process.env.PORT || 3000;
+
 app.listen(port);
 console.log('Express is running on: ' + port);
