705ddce00d9de02538df14d8c18636bdad2dc498
lnzd17 <lindsaykayedutton@gmail.com>
2017-09-16 09:38:09 -0400

    show page

--- controllers/movies.js
+++ controllers/movies.js
@@ -2,24 +2,33 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 
-
-router.get('/', (req, res) => {
+router
+.get('/', (req, res) => {
   const popularMoviesURL = `http://api.themoviedb.org/3/movie/popular?api_key=${process.env.MOVIE_API_KEY}&language=en-US&page=1`;
-
   axios.get(popularMoviesURL)
-    .then((api_res) => {
-      res.render('application', {
-        locals: {
-          movies: api_res.data
-        },
-        partials: {
-          yield: 'views/movies/index.html'
-        }
-      });
-    })
-    .catch((err) => {
-      return console.log(err);
+  .then((api_res) => {
+    res.render('application', {
+      locals: {
+        movies: api_res.data
+      },
+      partials: {
+        yield: 'views/movies/index.html'
+      }
     });
+  })
+  .catch((err) => {
+    return console.log(err);
   });
+})
+.get('/movies/:id', (req, res) => {
+  const movieURL = `http://api.themoviedb.org/3/movie/${req.params.id}?api_key=${process.env.MOVIE_API_KEY}&language=en-US`;
+  axios.get(movieURL)
+  .then((api_res) => {
+    res.send('hello');
+  }).catch((err) => {
+    return console.log(err);
+  });
+
+});
 
 module.exports = router;
